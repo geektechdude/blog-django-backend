6564652d7755b3744c35e3fa8f874c261f01b641
geektechdude <34283325+geektechdude@users.noreply.github.com>
2024-10-03 20:42:15 +0100

    initial models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from blog.models import Post, Profile, Tag
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+@admin.register(Post)
+class PostAdmin(admin.Model.Admin):
+    model = Post
+
+    list_display = (
+        "id",
+        "title",
+        "subitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+    list_filter = (
+        "published",
+        "publish_date",
+    )
+    list_editable = (
+        "title",
+        "subtitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+    search_fields = (
+        "title",
+        "subtitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+    prepopulated_fields = {
+        "slug": {
+            "title",
+            "subtitle",
+        }
+    }
+    date_hierachy = "publish_date"
+    save_on_top = True
