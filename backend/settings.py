6f00febab6b31630fa93372dfb68efcbe5aabe79
geektechdude <34283325+geektechdude@users.noreply.github.com>
2024-10-04 19:02:00 +0100

    CORS Headers

--- backend/settings.py
+++ backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     "SCHEMA": "blog.schema.schema",
 }
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",)
