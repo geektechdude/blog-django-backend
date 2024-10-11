f823e3f7450af62b2cc7f8218d06bde1da49f928
geektechdude <34283325+geektechdude@users.noreply.github.com>
2024-10-11 22:37:13 +0100

    added environment options to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7p072d^gcv)ej23_u#d*rsyl8*s8#-xeys=kh^fq23zf)n3ox'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default='django-insecure-n7p072d^gcv)ej23_u#d*rsyl8*s8#-xeys=kh^fq23zf)n3ox')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", default=True)
 
 ALLOWED_HOSTS = []
 
@@ -79,8 +80,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("DJANGO_DB_ENGINE",
+                            default='django.db.backends.sqlite3'),
+        'NAME': os.getenv("DJANGO_DB_NAME",
+                          default=BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv("DJANGO_DB_USER", default='user'),
+        'PASSWORD': os.getenv("DJANGO_DB_PASS", default=''),
+        'HOST': os.getenv("DJANGO_DB_HOST", default='localhost'),
+        'PORT': os.getenv("DJANGO_DB_PORT", default='')
     }
 }
 
